7907dce96cdf23327d8b5e96a220b6a3c6491196
Neeraj Dalal <admin@nrjdalal.com>
2025-05-27 13:59:34 +0530

    chore: tweaks > __root.tsx
    src/app/__root.tsx

--- src/app/__root.tsx
+++ src/app/__root.tsx
@@ -9,6 +9,13 @@ export const Route = createRootRoute({
   head: () => ({
     meta: [
       {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
         title: "TanStack Start Starter",
       },
     ],
