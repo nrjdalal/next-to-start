5957b9d4e77dadb547fc17a5bcb64bc660f36fa6
Neeraj Dalal <admin@nrjdalal.com>
2025-05-30 01:45:16 +0530

    chore: tweaks > index.tsx
    src/app/index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,25 @@
 export const Route = createFileRoute({
-  component: Page,
-  loader: async () => {
-    const res = await fetch("https://api.vercel.app/blog")
-    return await res.json()
-  },
+  component: Home,
 })
 
-function Page() {
-  const posts = Route.useLoaderData()
-
+function Home() {
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
+    <main className="flex min-h-dvh w-screen flex-col items-center justify-center gap-y-4 p-4">
+      <img
+        className="w-full max-w-sm"
+        src="https://tanstack.com/assets/splash-dark-8nwlc0Nt.png"
+        alt="TanStack Logo"
+      />
+      <h1>
+        <span className="line-through">Next.js</span> TanStack Start
+      </h1>
+      <a
+        className="bg-foreground text-background rounded-full px-4 py-1 hover:opacity-90"
+        href="https://tanstack.com/start/latest"
+        target="_blank"
+      >
+        Docs
+      </a>
+    </main>
   )
 }
